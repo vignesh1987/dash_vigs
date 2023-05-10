234bcc46ca4f98d0c85a5356855d6526f2c029d4
vignesh1987 <33900146+vignesh1987@users.noreply.github.com>
2023-05-10 23:58:31 +0100

    Update dash_vig.py

--- dash_vig.py
+++ dash_vig.py
@@ -1,22 +1,44 @@
+#Importing libraries
 import base64
 import datetime
 import io
 
 import dash
 from dash.dependencies import Input, Output, State
-from dash import dcc, html, dash_table
+from dash import dcc, html
+import dash_table
+import plotly.express as px
 
 import pandas as pd
 
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
+                suppress_callback_exceptions=True)
 server=app.server
 app.layout = html.Div([
+    # this code section taken from Dash docs https://dash.plotly.com/dash-core-components/upload
+    html.Div(
+            [    html.H3(
+                'Laos Energy model',
+                style={"margin-bottom": "1px"},
+                 ),
+                 html.H5(
+                "OSeMOSYS application",
+                style={"margin-top": "0px"},
+                 ),
+            ],
+            
+            id="title",
+            ),
+    html.Hr(className='row'),
+    html.Hr(className='row'),
+    
     dcc.Upload(
         id='upload-data',
         children=html.Div([
-            'Drag and Drop or ',
+            'Please uplaod the Result CSV for "total Capacity Annual" :  Drag and Drop or ',
             html.A('Select Files')
         ]),
         style={
@@ -32,8 +54,10 @@ app.layout = html.Div([
         # Allow multiple files to be uploaded
         multiple=True
     ),
-    html.Div(id='output-data-upload'),
+    html.Div(id='output-div'),
+    html.Div(id='output-datatable'),
 ])
+
 
 def parse_contents(contents, filename, date):
     content_type, content_string = contents.split(',')
@@ -56,11 +80,14 @@ def parse_contents(contents, filename, date):
     return html.Div([
         html.H5(filename),
         html.H6(datetime.datetime.fromtimestamp(date)),
-
+        html.Button(id="submit-button", children="Create Graph"),
+        html.Hr(),
         dash_table.DataTable(
-            df.to_dict('records'),
-            [{'name': i, 'id': i} for i in df.columns]
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns],
+            page_size=15
         ),
+        dcc.Store(id='stored-data', data=df.to_dict('records')),
 
         html.Hr(),  # horizontal line
 
@@ -72,7 +99,7 @@ def parse_contents(contents, filename, date):
         })
     ])
 
-@app.callback(Output('output-data-upload', 'children'),
+@app.callback(Output('output-datatable', 'children'),
               Input('upload-data', 'contents'),
               State('upload-data', 'filename'),
               State('upload-data', 'last_modified'))
@@ -83,5 +110,32 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
             zip(list_of_contents, list_of_names, list_of_dates)]
         return children
 
+
+@app.callback(Output('output-div', 'children'),
+              Input('submit-button','n_clicks'),
+              State('stored-data','data'))
+
+def make_graphs(n, data):
+    if n is None:
+        return dash.no_update
+    else:
+        #d1=data[(data['t'].str[:3]=="PWR")]
+        #print(data[2])
+        #filtered_data = []
+        #for index in range(len(data)):
+         #   for key in data[index]:
+          #      print(key)
+           #     print(data[index][key])
+        totalcapacityannual=pd.DataFrame(data)
+        totalcapacityannual=totalcapacityannual[(totalcapacityannual['t'].str[:3]=="PWR") 
+                                                 & (totalcapacityannual['t'].str[3:6]!=("TRN")) 
+                                                 & (totalcapacityannual['t'].str[3:6]!=("DIS"))].copy()
+        totalcapacityannual.drop(('r'),axis=1,inplace=True)
+        bar_fig= px.bar(totalcapacityannual, x='y', y='TotalCapacityAnnual', width=1200, height=400, color='t', barmode='stack',title="Total Annual Capacity_Lao PDR-GW")
+        # print(data)
+        return dcc.Graph(figure=bar_fig)
+
+
+
 if __name__ == '__main__':
-    app.run_server(debug=True, use_reloader=false)
+    app.run_server(debug=True, use_reloader=False)
